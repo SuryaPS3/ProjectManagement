f6a24b54010e4d17a905a498d1aea76dd1eaf6d4
SuryaPS3 <spsingh2003.sps@gmail.com>
2025-11-04 22:16:49 +0530

    Completing everything after the prev commit

--- server/server.js
+++ server/server.js
@@ -1,13 +1,13 @@
 import express from "express";
 import "dotenv/config";
 import cors from "cors";
-
+import { clerkMiddleware } from "@clerk/express";
 const app = express();
 
 //Midlleware/ parses incoming JSON requests and puts the parsed data in req.body
 app.use(express.json());
 app.use(cors());
-
+app.use(clerkMiddleware())
 //route
 app.get("/",(req,res)=>res.send("First Route: Server is running"));
 
